ffaf75c809151fa47b40ce4b35751d8665c68d84
JHoangSMU <hvdthong@gmail.com>
2018-10-29 12:23:17 +0800

    adding files

--- nn/two_layer_net_module.py
+++ nn/two_layer_net_module.py
@@ -9,25 +9,27 @@ want a model more complex than a simple sequence of existing Modules you will
 need to define your model this way.
 """
 
-class TwoLayerNet(torch.nn.Module):
-  def __init__(self, D_in, H, D_out):
-    """
-    In the constructor we instantiate two nn.Linear modules and assign them as
-    member variables.
-    """
-    super(TwoLayerNet, self).__init__()
-    self.linear1 = torch.nn.Linear(D_in, H)
-    self.linear2 = torch.nn.Linear(H, D_out)
 
-  def forward(self, x):
-    """
-    In the forward function we accept a Tensor of input data and we must return
-    a Tensor of output data. We can use Modules defined in the constructor as
-    well as arbitrary (differentiable) operations on Tensors.
-    """
-    h_relu = self.linear1(x).clamp(min=0)
-    y_pred = self.linear2(h_relu)
-    return y_pred
+class TwoLayerNet(torch.nn.Module):
+    def __init__(self, D_in, H, D_out):
+        """
+        In the constructor we instantiate two nn.Linear modules and assign them as
+        member variables.
+        """
+        super(TwoLayerNet, self).__init__()
+        self.linear1 = torch.nn.Linear(D_in, H)
+        self.linear2 = torch.nn.Linear(H, D_out)
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary (differentiable) operations on Tensors.
+        """
+        h_relu = self.linear1(x).clamp(min=0)
+        y_pred = self.linear2(h_relu)
+        return y_pred
+
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
@@ -46,15 +48,14 @@ model = TwoLayerNet(D_in, H, D_out)
 loss_fn = torch.nn.MSELoss(reduction='sum')
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 for t in range(500):
-  # Forward pass: Compute predicted y by passing x to the model
-  y_pred = model(x)
+    # Forward pass: Compute predicted y by passing x to the model
+    y_pred = model(x)
 
-  # Compute and print loss
-  loss = loss_fn(y_pred, y)
-  print(t, loss.item())
+    # Compute and print loss
+    loss = loss_fn(y_pred, y)
+    print(t, loss.item())
 
-  # Zero gradients, perform a backward pass, and update the weights.
-  optimizer.zero_grad()
-  loss.backward()
-  optimizer.step()
-
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
